b4b8162fb83a70e451f28bd05a672f62e089fe4c
tdp1999 <tdp99.business@gmail.com>
2022-06-15 18:17:52 +0700

    try to fix pg connections strring bug

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,27 @@
 // Update with your config settings.
 const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+config.ssl = { rejectUnauthorized: false };
 
-if (process.env.DB_ENVIRONMENT === 'production') {
-	const config = parse(process.env.DATABASE_URL);
-	config.ssl = { rejectUnauthorized: false };
-
-	module.exports = {
-		production: {
-			client: 'pg',
-			connection: config,
-			pool: {
-				min: 2,
-				max: 10,
-			},
-			migrations: {
-				tableName: 'knex_migrations',
-				directory: './migrations',
-			},
-		},
-	};
-}
+// if (process.env.DB_ENVIRONMENT === 'development') {
+// 	module.exports = {
+// 		development: {
+// 			client: 'postgresql',
+// 			connection: {
+// 				database: 'checklist',
+// 				user: 'postgres',
+// 				password: '07102464024',
+// 			},
+// 			pool: {
+// 				min: 2,
+// 				max: 10,
+// 			},
+// 			migrations: {
+// 				tableName: 'knex_migrations',
+// 			},
+// 		},
+// 	};
+// }
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -40,16 +42,16 @@ module.exports = {
 			tableName: 'knex_migrations',
 		},
 	},
-	// production: {
-	// 	client: 'pg',
-	// 	connection: config,
-	// 	pool: {
-	// 		min: 2,
-	// 		max: 10,
-	// 	},
-	// 	migrations: {
-	// 		tableName: 'knex_migrations',
-	// 		directory: './migrations',
-	// 	},
-	// },
+	production: {
+		client: 'pg',
+		connection: config,
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: 'knex_migrations',
+			directory: './migrations',
+		},
+	},
 };
